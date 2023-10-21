b12763048dd28fc8d75934fe71fcacdc5bd48b7a
Elim Florvil <elim.florvil@icloud.com>
2023-10-21 15:16:51 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env', // Chemin vers le fichier .env
+      isGlobal: true, // Indique que la configuration est globale et peut être utilisée partout dans l'application
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

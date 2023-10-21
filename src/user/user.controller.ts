b7f750c3f39025df4959f2e8c22f5fa3e4a08134
Elim Florvil <elim.florvil@icloud.com>
2023-10-21 15:17:11 +0200

    Create user.controller.ts

---
+++ src/user/user.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Post, Get, Param, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './schemas/user.shema';
+import { ObjectId } from 'mongoose';
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get()
+  async getAllUser(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+  @Post()
+  async createUser(
+    @Body()
+    user: CreateUserDto,
+  ): Promise<User> {
+    return this.userService.createUser(user);
+  }
+
+  @Get(':id')
+  async getUser(
+    @Param('id')
+    id: string,
+  ): Promise<User> {
+    return this.userService.findOneById(id);
+  }
+  @Delete(':id')
+  async deleteUser(
+    @Param('id')
+    id: ObjectId,
+  ): Promise<User> {
+    return this.userService.deleteOneById(id);
+  }
+}

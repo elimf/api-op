ef00ba4e06c556aec2c40b293b856ab50d6d1984
Elim Florvil <elim.florvil@icloud.com>
2023-10-25 00:38:59 +0200

    add swagger on api

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('One Piece API')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('One Piece')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
   await app.listen(process.env.NODE_PORT);
 }
 bootstrap();

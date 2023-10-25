896be7f4f970adda779dde1745ba8d603c12ad4e
Elim Florvil <elim.florvil@ceo-vision.com>
2023-10-25 10:15:15 +0200

    update the schema with a settings role and update the token system

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,8 +24,11 @@ export class AuthService {
 
   async login(user: User) {
     const payload = {
-      username: user.email,
-      sub: user.username,
+      email: user.email,
+      role: user.role,
+      sub: {
+        username: user.username,
+      },
     };
     return {
       access_token: await this.jwtService.sign(payload, {
@@ -39,9 +42,12 @@ export class AuthService {
   }
   async refreshToken(user: User) {
     const payload = {
-      username: user.email,
-      sub: user.username,
-    };
+      email: user.email,
+      role: user.role,
+      sub: {
+        username: user.username,
+      },
+    };    
     return {
       access_token: await this.jwtService.sign(payload, {
         secret: process.env.JWT_SECRET,

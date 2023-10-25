5b7ca9949a40d336937fce42ed7de5f0ba1f609d
Elim Florvil <elim.florvil@icloud.com>
2023-10-26 00:23:07 +0200

    Update create-arc.dto.ts

--- src/arc/dto/create-arc.dto.ts
+++ src/arc/dto/create-arc.dto.ts
@@ -1,1 +1,27 @@
-export class CreateArcDto {}
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Character } from 'src/character/schema/character.schema';
+import { Event } from 'src/event/schema/event.schema';
+
+export class CreateArcDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  level_required: number;
+
+  @ApiProperty({ type: [Character] })
+  characters?: Character[];
+
+  @ApiProperty({ type: [Event] })
+  events?: Event[];
+}

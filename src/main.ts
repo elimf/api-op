1583f7c93662d0bc11d729ab9605376b0ed6f350
Elim Florvil <elim.florvil@icloud.com>
2023-10-28 00:39:01 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,28 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  const config = new DocumentBuilder()
+  const swaggerOptions = new DocumentBuilder()
     .setTitle('One Piece API')
     .setDescription('Api of the One Piece adventure ')
     .setVersion('1.0')
-    .addTag('One Piece')
+    .addBearerAuth()
+    .setLicense('MIT License', 'https://opensource.org/licenses/MIT')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  // Define custom Swagger UI configuration
+  const swaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customCss:
+      '.swagger-ui .topbar { background-color: #1b1b1b} .try-out__btn {background-color: #007acc !important;color: #fff !important;}',
+    customSiteTitle: 'api-op-production.up.railway.app',
+    customfavIcon:
+      'https://static.vecteezy.com/system/resources/previews/023/507/705/original/one-piece-logo-heart-pirate-fanart-free-png.png',
+  };
+
+  SwaggerModule.setup('', app, document, swaggerCustomOptions);
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }

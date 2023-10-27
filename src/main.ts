c77259311a59755a739c3f02b08eef0a35863295
Elim Florvil <elim.florvil@icloud.com>
2023-10-27 23:41:48 +0200

    recup main

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
-  await app.listen(process.env.NODE_PORT);
+  await app.listen(process.env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

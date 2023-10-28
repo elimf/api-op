c1295527a0999df99777cca2ed88bc0fd0735337
Elim Florvil <elim.florvil@icloud.com>
2023-10-28 15:53:46 +0200

    update swagger

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     .addBearerAuth()
     .setLicense('MIT License', 'https://opensource.org/licenses/MIT')
     .build();
+
   const document = SwaggerModule.createDocument(app, swaggerOptions);
 
   // Define custom Swagger UI configuration
@@ -26,15 +27,13 @@ async function bootstrap() {
     swaggerOptions: {
       persistAuthorization: true,
     },
-    customCss:
-      '.swagger-ui .topbar { background-color: #1b1b1b} .try-out__btn {background-color: #007acc !important;color: #fff !important;}',
-    customSiteTitle: 'api-op-production.up.railway.app',
+    customSiteTitle: 'Api OnePiece Production',
     customfavIcon:
       'https://static.vecteezy.com/system/resources/previews/023/507/705/original/one-piece-logo-heart-pirate-fanart-free-png.png',
   };
 
   SwaggerModule.setup('', app, document, swaggerCustomOptions);
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

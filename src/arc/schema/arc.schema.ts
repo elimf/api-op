44c666c4a1a520dc443e53127a2681de5a657b31
Elim Florvil <elim.florvil@icloud.com>
2023-10-28 19:16:06 +0200

    Update arc.schema.ts

--- src/arc/schema/arc.schema.ts
+++ src/arc/schema/arc.schema.ts
@@ -1,35 +1,45 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
 
 @Schema()
 export class Arc extends Document {
   @Prop({ required: true })
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'the umpteenth arc in the work knowing that the FILLERS are at 0 ',
+  })
   number: number;
 
   @Prop()
-  @ApiProperty()
+  @ApiProperty({ description: 'Level required to unlock the arc' })
   level_required: number;
 
   @Prop({ required: true })
-  @ApiProperty()
+  @ApiProperty({ description: 'Arc name' })
   name: string;
 
   @Prop()
-  @ApiProperty()
+  @ApiProperty({ description: 'Arc description' })
   description: string;
 
-  @Prop({ type: [{ type: [String], ref: 'Character' }] })
-  @ApiProperty({ type: [String] })
-  characters_to_unlock: string[];
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Character' }] })
+  @ApiProperty({
+    type: [String],
+    description: 'Unlockable Character ID Table',
+  })
+  characters_to_unlock: [];
 
-  @Prop({ type: [String], ref: 'Event' })
-  @ApiProperty({ type: [String] })
-  event_to_unlock: string[];
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }] })
+  @ApiProperty({
+    description: 'Events ID table to unlock',
+    type: [String],
+  })
+  event_to_unlock: [];
 
-  @Prop()
-  @ApiProperty()
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Saga' })
+  @ApiProperty({ type: String, description: 'ID of the associated saga' })
   saga_id: string;
 }
 

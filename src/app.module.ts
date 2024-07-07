ed68ca4eab8f99d629744691a73073dd2e3891fa
elimf <90334513+elimf@users.noreply.github.com>
2024-07-07 07:06:10 +0200

    chore: Update npm dependencies for NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,14 @@ import { SagasModule } from './sagas/sagas.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'onepiece',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Chemin vers les entités
-      synchronize: true, // Synchronisation automatique du schéma (attention en production)
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     SagasModule,
   ],
